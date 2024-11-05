73350bfdfec7068632474adcb8e85caa23567794
Ayala <ayalazhk@gmail.com>
2024-11-05 07:10:28 +0500

    Женілдік, сан аралығы және теріс санды тексеруді қостым

---
+++ hw1-week10-JS/script.js
@@ -0,0 +1,16 @@
+//1
+let age = 65;
+let isStudent = false
+
+console.log(age > 65 || isStudent);
+
+//2
+let number = Number(prompt("Санды енгізіңіз:"))
+
+alert(number >=10 && number <= 20);
+
+//3
+let num1 = -5;
+let num2 = 10;
+
+console.log(num1 <0 && num2>=0) || (num1>=0 && num2 <0);
